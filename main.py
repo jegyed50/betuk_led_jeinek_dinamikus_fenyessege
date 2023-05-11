8670688f116089b8b47ab5f1c4d879c07110bd32
Egyed József <jegyed50@gmail.com>
2023-05-11 16:38:29 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-led.set_brightness(15)
+currentledbrihtness = 0
+led.set_brightness(100)
 led.set_display_mode(DisplayMode.GREYSCALE)
 basic.show_string("W")
 
-
-
-def on_in_background2():
-    for x in range(5):
-        for y in range(5):
-            if led.point_brightness(x, y) > 10:
-                for valtozofenyesseg in range(26):
-                    led.plot_brightness(x, y, valtozofenyesseg * 10)
-                led.plot(x, y)
-            else:
-                pass
-control.in_background(on_in_background2)
+def on_in_background():
+    global currentledbrihtness
+    while True:
+        for x in range(5):
+            for y in range(5):
+                currentledbrihtness = led.point_brightness(x, y)
+                if currentledbrihtness > 10:
+                    for valtozofenyesseg in range(26):
+                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
+                        basic.pause(valtozofenyesseg * 2)
+                    led.plot_brightness(x, y, 255)
+                    for valtozofenyesseg2 in range(26):
+                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg2 * 10)
+                        basic.pause(valtozofenyesseg2 * 2)
+                    led.plot_brightness(x, y, currentledbrihtness)
+                else:
+                    currentledbrihtness = 124
+                    for valtozofenyesseg3 in range(26):
+                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg3 * 10)
+                        basic.pause(valtozofenyesseg3 * 2)
+                    for valtozofenyesseg4 in range(26):
+                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg4 * 1)
+                        basic.pause(valtozofenyesseg4 * 2)
+                    led.plot_brightness(x, y, 0)
+control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,32 +1,28 @@
 let currentledbrihtness = 0
-led.setBrightness(255)
+led.setBrightness(150)
 led.setDisplayMode(DisplayMode.Greyscale)
-basic.showString("W")
+basic.showString("T")
 control.inBackground(function () {
     while (true) {
         for (let x = 0; x <= 4; x++) {
             for (let y = 0; y <= 4; y++) {
                 currentledbrihtness = led.pointBrightness(x, y)
-                if (currentledbrihtness > 10) {
+                if (currentledbrihtness > 0) {
                     for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
                         led.plotBrightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
-                        basic.pause(20)
+                        basic.pause(valtozofenyesseg * 2)
                     }
                     led.plotBrightness(x, y, 255)
                     for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
-                        led.plotBrightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
-                        basic.pause(20)
+                        led.plotBrightness(x, y, currentledbrihtness - 0 * 10)
+                        basic.pause(valtozofenyesseg * 2)
                     }
                     led.plotBrightness(x, y, currentledbrihtness)
                 } else {
                     currentledbrihtness = 124
                     for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
                         led.plotBrightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
-                        basic.pause(20)
-                    }
-                    for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
-                        led.plotBrightness(x, y, currentledbrihtness - valtozofenyesseg * 1)
-                        basic.pause(20)
+                        basic.pause(valtozofenyesseg * 2)
                     }
                     led.plotBrightness(x, y, 0)
                 }
