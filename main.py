f499fb483b1e4fba47e4b68a90a6e18cd5f77adb
Egyed József <jegyed50@gmail.com>
2023-05-12 10:47:26 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
-currentledbrihtness = 0
-led.set_brightness(100)
-led.set_display_mode(DisplayMode.GREYSCALE)
-basic.show_string("W")
-
-def on_in_background():
+def ej_screensaver(switch_off_LED_max_brightness: number, fade_delay: number):
     global currentledbrihtness
     while True:
         for x in range(5):
             for y in range(5):
                 currentledbrihtness = led.point_brightness(x, y)
-                if currentledbrihtness > 10:
+                if currentledbrihtness > 0:
                     for valtozofenyesseg in range(26):
                         led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
                         basic.pause(valtozofenyesseg * 2)
                     led.plot_brightness(x, y, 255)
                     for valtozofenyesseg2 in range(26):
-                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg2 * 10)
+                        led.plot_brightness(x, y, currentledbrihtness - 0 * 10)
                         basic.pause(valtozofenyesseg2 * 2)
                     led.plot_brightness(x, y, currentledbrihtness)
                 else:
-                    currentledbrihtness = 124
-                    for valtozofenyesseg3 in range(26):
-                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg3 * 10)
-                        basic.pause(valtozofenyesseg3 * 2)
-                    for valtozofenyesseg4 in range(26):
-                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg4 * 1)
-                        basic.pause(valtozofenyesseg4 * 2)
-                    led.plot_brightness(x, y, 0)
+                    pass
+currentledbrihtness = 0
+led.set_brightness(150)
+led.set_display_mode(DisplayMode.GREYSCALE)
+basic.show_string("T")
+
+def on_in_background():
+    ej_screensaver(255, 10)
 control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
-let currentledbrihtness = 0
-led.setBrightness(150)
-led.setDisplayMode(DisplayMode.Greyscale)
-basic.showString("T")
-control.inBackground(function () {
+// Funcion call: ejscreensaver(n1,n2).
+// The function calls insert to "run in bacground" block.
+function ej_screensaver (switch_off_LED_max_brightness: number, fade_delay: number) {
     while (true) {
         for (let x = 0; x <= 4; x++) {
             for (let y = 0; y <= 4; y++) {
-                currentledbrihtness = led.pointBrightness(x, y)
-                if (currentledbrihtness > 0) {
+                currentledbrightness = led.pointBrightness(x, y)
+                if (currentledbrightness > 0) {
                     for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
-                        led.plotBrightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
-                        basic.pause(valtozofenyesseg * 2)
+                        led.plotBrightness(x, y, currentledbrightness - valtozofenyesseg * 10)
+                        basic.pause(valtozofenyesseg * fade_delay)
                     }
                     led.plotBrightness(x, y, 255)
                     for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
-                        led.plotBrightness(x, y, currentledbrihtness - 0 * 10)
-                        basic.pause(valtozofenyesseg * 2)
+                        led.plotBrightness(x, y, currentledbrightness - 0 * 10)
+                        basic.pause(valtozofenyesseg * fade_delay)
                     }
-                    led.plotBrightness(x, y, currentledbrihtness)
+                    led.plotBrightness(x, y, currentledbrightness)
                 } else {
-                    currentledbrihtness = 124
-                    for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
-                        led.plotBrightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
-                        basic.pause(valtozofenyesseg * 2)
+                    for (let valtozofenyesseg = 0; valtozofenyesseg <= switch_off_LED_max_brightness / 25; valtozofenyesseg++) {
+                        led.plotBrightness(x, y, valtozofenyesseg * 25)
+                        basic.pause(valtozofenyesseg * fade_delay * 10)
                     }
                     led.plotBrightness(x, y, 0)
+                    basic.pause(fade_delay * 100)
                 }
             }
         }
     }
+}
+let currentledbrightness = 0
+led.setBrightness(150)
+led.setDisplayMode(DisplayMode.Greyscale)
+basic.showString("T")
+control.inBackground(function () {
+    ej_screensaver(255, 3)
 })
