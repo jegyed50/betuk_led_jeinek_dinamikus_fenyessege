de63976fad5bfdb65aa2fac4e851f4a83e5497e7
Egyed József <jegyed50@gmail.com>
2023-05-14 07:27:38 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+# Funcion call: ej_screensaver(n1,n2).
+# The function calls insert to "run in bacground" block.
 def ej_screensaver(switch_off_LED_max_brightness: number, fade_delay: number):
-    global currentledbrihtness
+    global currentledbrightness
     while True:
         for x in range(5):
             for y in range(5):
-                currentledbrihtness = led.point_brightness(x, y)
-                if currentledbrihtness > 0:
+                currentledbrightness = led.point_brightness(x, y)
+                if currentledbrightness > 0:
                     for valtozofenyesseg in range(26):
-                        led.plot_brightness(x, y, currentledbrihtness - valtozofenyesseg * 10)
-                        basic.pause(valtozofenyesseg * 2)
+                        led.plot_brightness(x, y, currentledbrightness - valtozofenyesseg * 10)
+                        basic.pause(valtozofenyesseg * fade_delay)
                     led.plot_brightness(x, y, 255)
                     for valtozofenyesseg2 in range(26):
-                        led.plot_brightness(x, y, currentledbrihtness - 0 * 10)
-                        basic.pause(valtozofenyesseg2 * 2)
-                    led.plot_brightness(x, y, currentledbrihtness)
+                        led.plot_brightness(x, y, currentledbrightness - 0 * 10)
+                        basic.pause(valtozofenyesseg2 * fade_delay)
+                    led.plot_brightness(x, y, currentledbrightness)
                 else:
-                    pass
-currentledbrihtness = 0
+                    valtozofenyesseg3 = 0
+                    while valtozofenyesseg3 <= switch_off_LED_max_brightness / 25:
+                        led.plot_brightness(x, y, valtozofenyesseg3 * 25)
+                        basic.pause(valtozofenyesseg3 * fade_delay * 10)
+                        valtozofenyesseg3 += 1
+                    led.plot_brightness(x, y, 0)
+                    basic.pause(fade_delay * 100)
+currentledbrightness = 0
 led.set_brightness(150)
 led.set_display_mode(DisplayMode.GREYSCALE)
 basic.show_string("T")
 
 def on_in_background():
-    ej_screensaver(255, 10)
+    ej_screensaver(255, 3)
 control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-// Funcion call: ejscreensaver(n1,n2).
+// Funcion call: ej_screensaver(n1,n2).
 // The function calls insert to "run in bacground" block.
 function ej_screensaver (switch_off_LED_max_brightness: number, fade_delay: number) {
-    while (true) {
+    let valtozofenyesseg3: number;
+while (true) {
         for (let x = 0; x <= 4; x++) {
             for (let y = 0; y <= 4; y++) {
                 currentledbrightness = led.pointBrightness(x, y)
@@ -11,15 +12,17 @@ function ej_screensaver (switch_off_LED_max_brightness: number, fade_delay: numb
                         basic.pause(valtozofenyesseg * fade_delay)
                     }
                     led.plotBrightness(x, y, 255)
-                    for (let valtozofenyesseg = 0; valtozofenyesseg <= 25; valtozofenyesseg++) {
+                    for (let valtozofenyesseg2 = 0; valtozofenyesseg2 <= 25; valtozofenyesseg2++) {
                         led.plotBrightness(x, y, currentledbrightness - 0 * 10)
-                        basic.pause(valtozofenyesseg * fade_delay)
+                        basic.pause(valtozofenyesseg2 * fade_delay)
                     }
                     led.plotBrightness(x, y, currentledbrightness)
                 } else {
-                    for (let valtozofenyesseg = 0; valtozofenyesseg <= switch_off_LED_max_brightness / 25; valtozofenyesseg++) {
-                        led.plotBrightness(x, y, valtozofenyesseg * 25)
-                        basic.pause(valtozofenyesseg * fade_delay * 10)
+                    valtozofenyesseg3 = 0
+                    while (valtozofenyesseg3 <= switch_off_LED_max_brightness / 25) {
+                        led.plotBrightness(x, y, valtozofenyesseg3 * 25)
+                        basic.pause(valtozofenyesseg3 * fade_delay * 10)
+                        valtozofenyesseg3 += 1
                     }
                     led.plotBrightness(x, y, 0)
                     basic.pause(fade_delay * 100)
